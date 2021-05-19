c1b7d52c1d8c1a3fd98be8ad7baf49dee2cced06
David Kloeren <d.kloeren@googlemail.com>
2021-05-19 15:43:30 +0200

    Add model validatation. Add Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'What\'s that smell?',
+  address: 'Cat street in Catland',
+  description: 'want to know how it is to live with a cat ... ? try it out here',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Inverse Tower',
+  address: '2000 miles under the sea',
+  description: "Don't forget a flashlight",
+  price_per_night: 150,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Bunker',
+  address: '10 Clifton Berlin 10002',
+  description: 'lalialalalioalalallai',
+  price_per_night: 50,
+  number_of_guests: 3
+)
